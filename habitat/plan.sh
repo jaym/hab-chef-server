5695b33ab2be1ef7293df7694fd365a7257bfd3f
Jay Mundrawala <jay@thechamberofunderstanding.com>
2018-06-06 10:48:51 -0500

    Add studiorc

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -4,20 +4,24 @@ pkg_type="composite"
 pkg_version="0.1.0"
  
 pkg_services=(
-    core/postgresql
-    core/elasticsearch5
+    # core/postgresql
+    # core/elasticsearch5
 
     chef/oc_id
     chef/bookshelf
     chef/oc_bifrost
     chef/oc_erchef
     chef/chef-server-nginx
+    chef/chef-server-ctl
 )
  
 pkg_bind_map=(
-   [chef/oc_id]="database:core/postgresql"
-   [chef/bookshelf]="database:core/postgresql"
-   [chef/oc_bifrost]="database:core/postgresql"
-   [chef/oc_erchef]="bookshelf:chef/bookshelf oc_bifrost:chef/oc_bifrost database:core/postgresql elasticsearch:core/elasticsearch5"
-   [chef/chef-server-nginx]="oc_erchef:chef/oc_erchef bookshelf:chef/bookshelf oc_bifrost:chef/oc_bifrost elasticsearch:core/elasticsearch5"
+   # [chef/oc_id]="database:core/postgresql"
+   # [chef/bookshelf]="database:core/postgresql"
+   # [chef/oc_bifrost]="database:core/postgresql"
+   [chef/oc_id]="chef-server-ctl:chef/chef-server-ctl"
+   [chef/bookshelf]="chef-server-ctl:chef/chef-server-ctl"
+   [chef/oc_bifrost]="chef-server-ctl:chef/chef-server-ctl"
+   [chef/oc_erchef]="bookshelf:chef/bookshelf oc_bifrost:chef/oc_bifrost chef-server-ctl:chef/chef-server-ctl"
+   [chef/chef-server-nginx]="oc_erchef:chef/oc_erchef bookshelf:chef/bookshelf oc_bifrost:chef/oc_bifrost chef-server-ctl:chef/chef-server-ctl"
 )
